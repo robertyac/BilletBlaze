e08effaf5c87c3c12a67464b96b37fba882449c5
Robert Yacovelli <ryacovel@student.ubc.ca>
2023-11-23 22:58:10 -0800

    Done FindBilletFragment

---
+++ app/src/main/java/com/example/billetblaze/MapsActivity.java
@@ -0,0 +1,101 @@
+package com.example.billetblaze;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.example.billetblaze.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Toast.makeText(this, "Long click on the map to pick a city", Toast.LENGTH_LONG).show();
+        // Enable the My Location layer
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+            // user location button
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+
+
+        // Set a long click listener for the map
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                // Handle the map long click
+                // For example, you can return the selected location to the previous activity
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("latitude", latLng.latitude);
+                resultIntent.putExtra("longitude", latLng.longitude);
+
+                // Convert lat long to city name
+                Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    if (addresses != null && !addresses.isEmpty()) {
+                        String city = addresses.get(0).getLocality();
+                        Log.d("MapsActivity", "City: " + city);
+                        resultIntent.putExtra("city", city);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+
+                // Provide haptic feedback
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vibrator != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
+                    }
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/billetblaze/ui/search/FindBilletFragment.java
+++ app/src/main/java/com/example/billetblaze/ui/search/FindBilletFragment.java
@@ -1,9 +1,20 @@
 package com.example.billetblaze.ui.search;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -11,13 +22,30 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.core.util.Pair;
+import androidx.navigation.Navigation;
+
+import com.example.billetblaze.MapsActivity;
 import com.example.billetblaze.R;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class FindBilletFragment extends Fragment {
 
     private Button destinationButton,datesButton,guestsButton,findBilletsButton;
     private TextView destinationTv,datesTv,guestsTv;
+    private String startDate, endDate, dateRange, city;
+    private int numGuestsInt;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +59,9 @@ public class FindBilletFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_find_billet, container, false);
 
+        // back button back to search fragment
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         destinationButton = view.findViewById(R.id.destinationButton);
         datesButton = view.findViewById(R.id.datesButton);
         guestsButton = view.findViewById(R.id.guestsButton);
@@ -38,10 +69,152 @@ public class FindBilletFragment extends Fragment {
         destinationTv = view.findViewById(R.id.destinationTv);
         datesTv = view.findViewById(R.id.datesTv);
         guestsTv = view.findViewById(R.id.guestsTv);
+        datesButton = view.findViewById(R.id.datesButton);
 
-        // Enable the back button
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getUserDestination();
+        getDateRange();
+        getGuests();
+        validateAndFindAvailableBillets();
 
         return view;
+    }
+    private void getUserDestination() {
+        ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            city = data.getStringExtra("city");
+                            Log.d("FindBilletFragment", "City: " + city);
+                            destinationTv.setText(city);
+                        }
+                    }
+                });
+
+        destinationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to launch the map
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                mStartForResult.launch(intent);
+            }
+        });
+    }
+    private void getDateRange() {
+        //MaterialDatePicker android devs
+        datesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+
+                // Get today's date
+                Calendar calendar = Calendar.getInstance(TimeZone.getDefault()); //get users time zone for current date
+                long today = calendar.getTimeInMillis();
+
+                // Set calendar constraints --> user can't pick a date range that starts in the past
+                CalendarConstraints.Builder constraintsBuilder = new CalendarConstraints.Builder();
+                constraintsBuilder.setStart(today);
+                constraintsBuilder.setValidator(DateValidatorPointForward.now());
+
+                builder.setCalendarConstraints(constraintsBuilder.build());
+
+                MaterialDatePicker<Pair<Long, Long>> picker = builder.build();
+                picker.show(getChildFragmentManager(), picker.toString());
+
+                //Get date range
+                picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+                    @Override
+                    public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                        // selection.first and selection.second in ms
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+                        sdf.setTimeZone(TimeZone.getDefault()); //set time zone to user's time zone
+                        // Add one day to the selected dates to adjust for the time zone difference
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTimeInMillis(selection.first);
+                        cal.add(Calendar.DAY_OF_MONTH, 1);
+                        startDate = sdf.format(cal.getTime());
+
+                        cal.setTimeInMillis(selection.second);
+                        cal.add(Calendar.DAY_OF_MONTH, 1);
+                        endDate = sdf.format(cal.getTime());
+
+                        dateRange = startDate + " to " + endDate;
+                        datesTv.setText(dateRange);
+                    }
+                });
+            }
+        });
+    }
+    private void getGuests() {
+        guestsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an AlertDialog.Builder
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Number of Guests");
+
+                // Set up the input
+                final EditText input = new EditText(getActivity());
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String numGuests = input.getText().toString();
+                        if (!numGuests.isEmpty()) {
+                            numGuestsInt = Integer.parseInt(numGuests);
+                            if (numGuestsInt > 0) {
+                                guestsTv.setText(numGuests);
+                            } else {
+                                Toast.makeText(getActivity(), "Please select a valid amount of guests.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Please select the amount of guests.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Show the dialog
+                builder.show();
+            }
+        });
+    }
+    private void validateAndFindAvailableBillets() {
+        findBilletsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (destinationTv.getText().toString().isEmpty()){
+                    Toast.makeText(getActivity(),"Please select a destination.",Toast.LENGTH_SHORT).show();
+                }
+                if (datesTv.getText().toString().isEmpty()){
+                    Toast.makeText(getActivity(),"Please select dates.",Toast.LENGTH_SHORT).show();
+                }
+                if (guestsTv.getText().toString().isEmpty()){
+                    Toast.makeText(getActivity(),"Please select the number of guests.",Toast.LENGTH_SHORT).show();
+                }
+
+                if (!destinationTv.getText().toString().isEmpty()
+                        && !datesTv.getText().toString().isEmpty()
+                        && !guestsTv.getText().toString().isEmpty()) {
+
+                    //TODO: need to pass: [dateRange city numGuestsInt] to the next fragment
+
+                    // All fields have been completed, navigate to the next fragment
+                    Navigation.findNavController(v).navigate(R.id.action_navigation_findBillet_to_navigation_searchResults);
+                }
+
+
+            }
+        });
     }
 }
