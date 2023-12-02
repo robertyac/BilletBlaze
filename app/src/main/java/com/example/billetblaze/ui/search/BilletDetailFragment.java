cd6633b0a7004feddecbfbd060ce7a6e43a30163
olivernicholass <omedgyesi@icloud.com>
2023-12-01 16:13:30 -0800

    Added price to BilletDetails + Adjustments.

--- app/src/main/java/com/example/billetblaze/ui/search/BilletDetailFragment.java
+++ app/src/main/java/com/example/billetblaze/ui/search/BilletDetailFragment.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import java.util.Random;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
@@ -15,7 +15,7 @@ import androidx.navigation.Navigation;
 import com.example.billetblaze.R;
 
 public class BilletDetailFragment extends Fragment {
-    private TextView billetTittleTv, billetDescriptionTV;
+    private TextView billetTittleTv, billetDescriptionTV, ppnTv, priceView;
     private Button bookButton;
     private ImageView billetImage;
     private String dateRange, city, desc;
@@ -26,10 +26,13 @@ public class BilletDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_billet_detail, container, false);
 
+        int price = priceGenerate(60, 200);
         billetTittleTv = view.findViewById(R.id.billetTittleTv);
         billetDescriptionTV = view.findViewById(R.id.billeDescriptionTV);
         bookButton = view.findViewById(R.id.bookButton);
         billetImage = view.findViewById(R.id.billetImage);
+        ppnTv = view.findViewById(R.id.ppnView);
+        priceView = view.findViewById(R.id.priceView);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -40,10 +43,12 @@ public class BilletDetailFragment extends Fragment {
 
         desc = "This spacious and comfortable house is located in a peaceful neighborhood. The house features three bedrooms, two bathrooms, a fully equipped kitchen, and a living room. " +
                 "The house is conveniently located close to local attractions and the local ESS station in " + city + ".";
-        billetDescriptionTV.setText("This Billet is available in " + city + "\n for the dates " + dateRange +
-                "\n for " + numGuests + " Guests.\n" + desc);
+        billetDescriptionTV.setText("This Billet is available in " + city + "\n\n - On the date " + dateRange +
+                "\n\n - For " + numGuests + " Guests.\n\n" + desc);
 
         billetTittleTv.setText("Cozy Billet in " + city);
+
+        priceView.setText("$ " + String.valueOf(price));
 
         // On selecting bookButton, we are taken to the personalInformation section.
         bookButton.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +58,10 @@ public class BilletDetailFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private int priceGenerate(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
