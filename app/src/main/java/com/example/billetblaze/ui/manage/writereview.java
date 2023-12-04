54f858069e5efe72c4f0898c48d2eabb37667f44
volumeOG <stefanspataro@hotmail.com>
2023-12-04 13:42:37 -0800

    added

--- app/src/main/java/com/example/billetblaze/ui/manage/writereview.java
+++ app/src/main/java/com/example/billetblaze/ui/manage/writereview.java
@@ -1,4 +1,4 @@
-package com.example.billetblaze;
+package com.example.billetblaze.ui.manage;
 
 import android.os.Bundle;
 
@@ -7,57 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link writereview#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.billetblaze.R;
+
+
 public class writereview extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public writereview() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment writereview.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static writereview newInstance(String param1, String param2) {
-        writereview fragment = new writereview();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private String review;
+    private Button submitButton;
+    private Button cancelButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_writereview, container, false);
+        submitButton = root.findViewById(R.id.button);
+        cancelButton = root.findViewById(R.id.button2);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_writereview, container, false);
     }
